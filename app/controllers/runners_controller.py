a75231858a8af44b063ae1ff0b3c6d384e332d63
stmcf <stevenmcfarlane@hotmail.co.uk>
2020-10-04 20:41:59 +0100

    Runners template completed, browser issues prevent full testing. templates set up in html file. No CSS added yet. Need to get browser rendering and routing properly first - tomorrow's problem.

--- app/controllers/runners_controller.py
+++ app/controllers/runners_controller.py
@@ -12,11 +12,42 @@ def runners():
     return render_template("runners/index.html", all_runners=runners)
 
 # NEW
+@runners_blueprint.route("/runners/new")
+def new_runner():
+    return render_template("/runners/new.html")
+# tested and working in browser
+# formatting required
 
 # CREATE
+@runners_blueprint.route("/runners/new", methods=["POST"])
+def create_runner():
+    first_name = request.form["first_name"]
+    last_name = request.form["last_name"]
+    new_runner = Runner(first_name, last_name, id)
+    runner_repository.save(new_runner)
+    return redirect("/runners")
+
 
 # EDIT
 
+@runners_blueprint.route('/runners/<id>/edit')
+def edit_runner(id):
+    runner = runner_repository.select(id)
+    return render_template("runner/edit.html, runner=runner")
+
 # UPDATE
 
+@runners_blueprint.route("runners/<id>", methods=["POST"])
+def update_runner(id):
+    first_name = request.form["first_name"]
+    last_name = request.form["last_name"]
+    new_runner = Runner(first_name, last_name, id)
+    runner_repository.save(new_runner)
+    return redirect("/runners")
+
 # DELETE
+
+@runners_blueprint.route("runners/<id>/delete", methods = ["POST"])
+def delete_runner(id):
+    runner_repository.delete(id)
+    return redirect ("/runners")
