7ca101de6b4c44777cd6fdbc7013670905e1c0d4
stmcf <stevenmcfarlane@hotmail.co.uk>
2020-10-04 13:12:42 +0100

    successfully render home page to browser.

--- app/app.py
+++ app/app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html' title = "Falkirk Trail Runners")
+    return render_template('index.html', title = "Falkirk Trail Runners")
 
 if __name__ =='__main__':
     app.run(debug=True)
---
+++ app/controllers/runners_controller.py
@@ -0,0 +1,18 @@
+from flask import Blueprint, Flask, redirect, render_template, request
+
+from models.runner import Runner
+import repositories.runner_repository as runner_repository
+
+runners_blueprint = Blueprint("runners", __name__)
+
+# INDEX
+
+# NEW
+
+# CREATE
+
+# EDIT
+
+# UPDATE
+
+# DELETE
